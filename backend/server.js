2a436256fee36d681f2d8e42496739437eb003de
Shubh-2402 <patilshubham852@gmail.com>
2022-01-23 00:21:43 +0530

    handled uncaught exceptions

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import dotenv from "dotenv";
 import app from "./app.js";
-
-// LOCAL IMPORTS
-
 import connectDB from "./config/database.js";
-
 dotenv.config({ path: "backend/config/config.env" });
+
+// Handle Uncaught exceptions
+
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down the server due to Uncaught exceptions");
+  server.close(() => {
+    process.exit(1);
+  });
+});
 
 // DB CONNECTION
 connectDB();
