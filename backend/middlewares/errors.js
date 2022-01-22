8f016ea82d9d41ca28a008c529f313f59acda7f7
Shubh-2402 <patilshubham852@gmail.com>
2022-01-23 00:41:16 +0530

    handled mongoose wrong objectid and validation errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -16,6 +16,20 @@ const errorMiddleware = (err, req, res, next) => {
     let error = { ...err };
     error.message = err.message;
 
+    //Handling Wrong Mongoose ObjectId Error
+
+    if (err.name === "CastError") {
+      const message = `Resource not found. Invalid ${err.path}`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    //Handling Mongoose Validation Error
+
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(error.statusCode).json({
       success: false,
       message: error.message || "Internal Server Error",
